8e36d2f2afcf998101666d9d60ad86af2648fe22
leonardo <leonardobiondi199@gmail.com>
2017-11-21 18:29:20 +0100

    modifica 3

--- provaGit.cpp
+++ provaGit.cpp
@@ -6,6 +6,11 @@
 #include<iostream>
 using namespace std;
 
+/**
+La funzione incrementa e stampa una variabile contatore
+@param int c --> contatore
+@return la stampa dei valori fino a 10 del contatore
+*/
 void incrementa(int c)
 {
 	for (int i = 0; i < 10; i++) {
@@ -14,6 +19,9 @@ void incrementa(int c)
 	}
 }
 
+/**
+Main dove viene richiamata la funzione incrementa()
+*/
 void main()
 {
 	int c = 0;
