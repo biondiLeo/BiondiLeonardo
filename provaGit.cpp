a7a91d6b8b3c31caab8ebbb0d593b056aab5de31
leonardo <leonardobiondi199@gmail.com>
2017-11-21 18:22:03 +0100

    modifica 1

---
+++ provaGit.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+
+void incrementa(int c)
+{
+	for (int i = 0; i < 10; i++) {
+		cout << "contatore: " << c << endl;
+		c++;
+	}
+}
+
+void main()
+{
+	int c = 0;
+	incrementa(c);
+	system("pause");
+}
